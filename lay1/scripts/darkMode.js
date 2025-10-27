5dba22cdb20f39ce74460b1943fdbaca0cd6556c
Jon4sSantos <jok4gegithub@gmail.com>
2025-10-27 16:13:37 -0300

    animação no botão

--- lay1/scripts/darkMode.js
+++ lay1/scripts/darkMode.js
@@ -1,21 +1,24 @@
 let ONorOFF_darkMode = false;
+let interruptor = document.getElementById('interruptor_darkMode'); 
 darkMode.addEventListener('click', function darkModeOn() {
     const root = document.documentElement;
 
-    if(!ONorOFF_darkMode) {
-    root.style.setProperty('--cor1', '#ffffff');
-    root.style.setProperty('--cor2', '#7d7d7d');
-    root.style.setProperty('--cor4', '#181818');
-    root.style.setProperty('--cor5', '#000000');
-    ONorOFF_darkMode = true;
+    if (!ONorOFF_darkMode) {
+        interruptor.style.left = '60%';
+        root.style.setProperty('--cor1', '#ffffff');
+        root.style.setProperty('--cor2', '#7d7d7d');
+        root.style.setProperty('--cor4', '#181818');
+        root.style.setProperty('--cor5', '#000000');
+        ONorOFF_darkMode = true;
     }
 
     else {
-    root.style.setProperty('--cor1', '#000000');
-    root.style.setProperty('--cor2', '#181818');
-    root.style.setProperty('--cor4', '#7d7d7d');
-    root.style.setProperty('--cor5', '#ffffff');
-    ONorOFF_darkMode = false;
+        interruptor.style.left = '0';
+        root.style.setProperty('--cor1', '#000000');
+        root.style.setProperty('--cor2', '#181818');
+        root.style.setProperty('--cor4', '#7d7d7d');
+        root.style.setProperty('--cor5', '#ffffff');
+        ONorOFF_darkMode = false;
     }
-    
+
 })
